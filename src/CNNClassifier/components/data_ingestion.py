102f6a8fd5583bc4ffe7f3bf3a9dc0b12afe7658
Aman123lug <ak06465676@gmail.com>
2023-07-21 20:05:23 +0530

    component and config added

---
+++ src/CNNClassifier/components/data_ingestion.py
@@ -0,0 +1,39 @@
+import os
+import urllib.request as request
+import zipfile
+from src.cnnClassifier import logger
+from src.cnnClassifier.utils.common import get_size
+from src.cnnClassifier.entity.config_entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig) -> None:
+        self.config = config
+    
+    def download_file(self):
+        try:
+            logger.info("Fetching zip file...")
+            if not os.path.exists(self.config.local_data_file):
+                filename, dirname = request.urlretrieve(
+                    url=self.config.source_URL,
+                    filename=self.config.local_data_file
+                )
+                logger.info(f"File Download Successfully {filename} with : {dirname}")
+                
+            else:
+                logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}") 
+                
+        except Exception as e:
+            logger.exception(e)
+            raise e
+            
+        
+    def extract_zip_file(self):
+        
+        unzip_dir = self.config.local_data_file
+
+        with zipfile.ZipFile(unzip_dir, "r") as unzip:
+            logger.info("zipefile read successfully")
+            unzip.extractall("data")
+            logger.info("zipfile extracted successfully")
+              
