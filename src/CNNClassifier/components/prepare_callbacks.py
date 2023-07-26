3a9318b4f132d2bf9164f20f8fdb815ad2bac760
Aman123lug <ak06465676@gmail.com>
2023-07-26 21:51:50 +0530

    model training error

---
+++ src/CNNClassifier/components/prepare_callbacks.py
@@ -0,0 +1,41 @@
+import time
+import tensorflow as tf
+from src.cnnClassifier import logger
+from cnnClassifier.entity.config_entity import PrepareCallbacksConfig
+import os
+
+
+
+class PrepareCallback:
+    def __init__(self, config: PrepareCallbacksConfig) -> None:
+        self.config = config
+    
+    @property
+    def _create_tb_callbacks(self):
+        timeStamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        
+        tb_running_log_dir = os.path.join(
+            self.config.tensorboard_root_log_dir,
+            f"tb_log_at_{timeStamp}",
+            
+        ) 
+        
+        return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
+    
+    @property
+    def _create_ckpt_callbacks(self):
+        
+        return tf.keras.callbacks.ModelCheckpoint(
+            self.config.checkpoint_model_filepath,
+            save_best_only=True
+        )
+        
+        
+    def _get_tb_ckpt_callbacks(self):
+        
+        return [
+            self._create_tb_callbacks,
+            self._create_ckpt_callbacks
+        ]
+        
+    
--- src/CNNClassifier/config/configration.py
+++ src/CNNClassifier/config/configration.py
@@ -1,8 +1,7 @@
-
 from cnnClassifier.constant import *
 from cnnClassifier.utils.common import load_bin, read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
-
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
+import os
 
 class ConfigurationManager:
     def __init__(self, configfile_path = CONFIG_FILE_PATH, paramsfile_path = PARAMS_FILE_PATH) -> None:
@@ -48,3 +47,27 @@ class ConfigurationManager:
         )
         
         return prepare_base_model_config
+    
+    
+    def get_prepare_callbacks_config(self) -> PrepareCallbacksConfig:
+        
+        config = self.config.prepare_callbacks
+        
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+        
+        prepare_callbacks_config = PrepareCallbacksConfig(
+            
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+            
+        )
+        
+        return prepare_callbacks_config
+    
+    
